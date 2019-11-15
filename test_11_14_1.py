3d9b77f401d691f0fba5b48b103098a883046892
PNUHC <luchunabf@163.com>
2019-11-15 16:41:03 +0800

    test_11_14_1-PNUHC

---
+++ test_11_14_1.py
@@ -0,0 +1,21 @@
+a = 'hehe'
+print(a)
+print(type(a))
+
+b = 3
+d = 4
+c1 = 'c1='
+c2 = 'c2='
+print(c1,end='')
+print((b)*(d))
+print(c2,end='')
+print((b)/(d))
+
+l = 4
+while l > 0:
+    print (l)
+    l -= 1
+while b!=0:
+    print ('I Love Phoner')
+    b -= 1
+
